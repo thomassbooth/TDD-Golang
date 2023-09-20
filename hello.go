6aa8d46d19a60058a1ed9d77e9062f8b6396ae8e
Thomas Booth <thomasbooth26@gmail.com>
2023-09-21 00:48:29 +0100

    new tests and added functionality

--- hello.go
+++ hello.go
@@ -2,10 +2,36 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
-	return "hello " + name
+const (
+	french             = "French"
+	spanish            = "Spanish"
+	englishHelloPrefix = "hello "
+	spanishHelloPrefix = "hola "
+	frenchHelloPrefix  = "bonjour "
+)
+
+func Hello(name string, language string) string {
+
+	if name == "" {
+		name = "world"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
--- hello_test.go
+++ hello_test.go
@@ -7,9 +7,41 @@ import "testing"
 //Test functions only take 1 Parameter: t *testing.T
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "hello Chris"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris", "")
+		want := "hello Chris"
 
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("Say, hello world when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "")
+		want := "hello world"
+
+		assertCorrectMessage(t, got, want)
+
+	})
+
+	t.Run("In spanish!", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "hola Elodie"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("In french!", func(t *testing.T) {
+		got := Hello("Dave", "French")
+		want := "bonjour Dave"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+// For helper functions, it's a good idea to accept a testing.TB
+// which is an interface that *testing.T and *testing.B
+func assertCorrectMessage(t testing.TB, got, want string) {
+
+	//t.Helper() tells the test suite that this method is a helper
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
