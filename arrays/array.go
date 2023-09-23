c2d5b80803beedec7aa9651d788d8037526439c7
Thomas Booth <thomasbooth26@gmail.com>
2023-09-23 01:48:09 +0100

    appending and making, range functon

--- arrays/array.go
+++ arrays/array.go
@@ -10,3 +10,39 @@ func Sum(numbers []int) int {
 
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	// lengthOfNumbers := len(numbersToSum)
+	// //make lets you create a slice of type first arg, with the length of 2nd arg
+	// sums := make([]int, lengthOfNumbers)
+
+	// //we loop over all
+	// for i, number := range numbersToSum {
+	// 	sums[i] = Sum(number)
+	// }
+
+	// return sums
+
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		//append to the sums array with 2nd value
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
+}
