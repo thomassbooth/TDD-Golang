4ffb6debed8f13cc8b845c565dd514d0ab088051
Thomas Booth <thomasbooth26@gmail.com>
2023-09-24 23:50:48 +0100

    Finishing up structs, methods and interfaces!

--- struc_method_interface/struc.go
+++ struc_method_interface/struc.go
@@ -1,15 +1,43 @@
 package struc
 
+import "math"
+
+// In Go interface resolution is implicit.
+// If the type you pass in matches what the interface is asking for, it will compile.
+type Shape interface {
+	Area() float64
+}
+
 // a struct is a named collection of fields where you can store data
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
 
-func Permiter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.Width + rectangle.Height)
+// a method is a function with a reciever, we can only call methods on types
+type Circle struct {
+	Radius float64
 }
 
-func Area(rectangle Rectangle) float64 {
-	return (rectangle.Width * rectangle.Height)
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+// the structure for a method is func (receiverName ReceiverType) MethodName(args)
+// It is convention in go for the reciever to be the first letter of the type
+func (r Rectangle) Area() float64 {
+	return (r.Width * r.Height)
+}
+
+func (t Triangle) Area() float64 {
+	return 0.5 * (t.Base * t.Height)
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func Permiter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
 }
--- struc_method_interface/struc_test.go
+++ struc_method_interface/struc_test.go
@@ -15,11 +15,47 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	rectangle := Rectangle{12.0, 6.0}
-	got := Area(rectangle)
-	want := 72.0
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	}
+	t.Run("rectangle", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 6.0}
+		checkArea(t, rectangle, 72.0)
+	})
 
-	if got != want {
-		t.Errorf("got %2.f want %.2f", got, want)
+	t.Run("circle", func(t *testing.T) {
+		circle := Circle{10}
+		checkArea(t, circle, 314.1592653589793)
+	})
+}
+
+func TestTableArea(t *testing.T) {
+
+	//here we are defining a slice of structs then we fill it in with the cases
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+
+		{name: "rectangle", shape: Rectangle{12, 6}, hasArea: 72.0},
+		{name: "circle", shape: Circle{10}, hasArea: 314.1592653589793},
+		{name: "triangle", shape: Triangle{12, 6}, hasArea: 36.0},
+	}
+
+	for _, tt := range areaTests {
+		// using tt.name from the case to use it as the `t.Run` test name
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				//%#v prints our structs with values in the field
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
+
 	}
 }
